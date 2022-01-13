0dc463d7d7af82e2f4cff17a579bb86a4cc0abc3
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-01-13 23:14:11 +0530

    Added Colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          'dark': '#000814',
+          'lightdark': '#001D3D',
+          'heropink': '#FFB0D4',
+          'deepblue': '#003566',
+          'oceanblue': '#0069C9',
+          'darkpurple': '#240046',
+          'deeppurple': '#3C096C',
+          'lightblue': '#59AFFF',
+          'purple': '#5A189A',
+          'semipurple': '#9D4EDD',
+          'lightpurple': '#C77DFF',
+          'pink': '#E8006A',
+          'semipink': '#FF479B',
+          'lightpink': '#FF80BA'
+        }
+      }
+    }
   },
   plugins: [],
 }
