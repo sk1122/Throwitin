cc65a0b4f5d0fe4cc4352f36c48b36ac4881652a
v3n0m <rahul_chaudhary@tutanota.com>
2022-01-19 19:47:00 +0530

    Investors and owners component flipping

---
+++ components/home/owner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Owner = (props: Props) => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      Owner Component
+    </div>
+  );
+};
+
+export default Owner;
