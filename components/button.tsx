4395fbcd30f46233b27a470507ddf87d4833da30
v3n0m <rahul_chaudhary@tutanota.com>
2022-01-31 21:39:41 +0530

    Minor Tweaks

--- components/button.tsx
+++ components/button.tsx
@@ -1,18 +1,23 @@
-import { useState, FC } from "react"
+import { useState, FC } from "react";
 
 type Props = {
-	gradient: boolean
-	customCss?: string
-}
+  gradient: boolean;
+  customCss?: string;
+};
 
 const Button: FC<Props> = ({ children, gradient, customCss }) => {
-	const [css, setCss] = useState(gradient ? 'bg-gradient-to-r text-white from-brand-heropink via-brand-lightpurple to-brand-lightblue' : 'bg-black text-white')
-	
-	return (
-		<div className={`font-poppins flex justify-center items-center py-2 w-36 px-2 h-10 rounded-lg cursor-pointer ${css} ${customCss}`}>
-			{children}
-		</div>
-	)
-}
+  const [css, setCss] = useState(
+    gradient
+      ? "bg-gradient-to-r text-white from-brand-heropink via-brand-lightpurple to-brand-lightblue"
+      : "bg-black text-white"
+  );
+
+  return (
+    <div
+      className={`font-poppins flex justify-center items-center py-2 w-36 px-2 h-10 rounded-lg cursor-pointer text-sm ${css} ${customCss}`}>
+      {children}
+    </div>
+  );
+};
 
 export default Button;
