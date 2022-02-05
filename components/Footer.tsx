924f4a4bbdac4a3415a4a56f4c7173370b3fc6f6
v3n0m <rahul_chaudhary@tutanota.com>
2022-02-05 17:16:36 +0530

    Minor tweaks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,26 +6,27 @@ const Footer = (props: Props) => {
       <div className="flex justify-around border-t-2 border-gray-700 pt-20">
         {/* left side */}
         <div className="flex flex-col w-full">
-          <h1 className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-brand-lightblue via-brand-purple to-brand-semipink">
+          <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-brand-lightblue via-brand-purple to-brand-semipink font-clash mb-3">
             THROWITIN
           </h1>
-          <p className="w-7/12">
+          <p className="w-7/12 text-gray-300">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nam
             ipsum vitae culpa exercitationem, facilis perspiciatis dolor ratione
             optio neque blanditiis corrupti iste necessitatibus placeat.
           </p>
         </div>
         {/* right side  */}
-        <div className="w-full flex justify-center">
-          <ul className="space-y-2 mr-28">
-            <li>Start Project</li>
-            <li>Fund project</li>
-            <li>About Us</li>
-            <li>Contact us</li>
+        <div className="w-full flex justify-center text-gray-300">
+          <ul className="space-y-3 mr-28">
+            {["Start Project", "Fund Project", "About us", "Contact us"].map(
+              (item) => (
+                <li className="cursor-pointer">{item}</li>
+              )
+            )}
           </ul>
-          <ul className="space-y-2">
-            <li>Privacy Policy</li>
-            <li>Terms & Conditions</li>
+          <ul className="space-y-3">
+            <li className="cursor-pointer">Privacy Policy</li>
+            <li className="cursor-pointer">Terms & Conditions</li>
           </ul>
         </div>
       </div>
