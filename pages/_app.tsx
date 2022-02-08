76e5798276463f8359dcb1fee928ea16bf451104
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-02-08 17:13:38 +0530

    Added ConnectWallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from "next/app";
 import { AppContext } from "./_context";
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import abi from './interface/contract.json'
+import { ethers } from "ethers";
+import { Project } from "./types/project";
+import { ThirdwebProvider } from "@3rdweb/react";
 
 declare global {
   interface Window {
@@ -10,10 +14,24 @@ declare global {
   }
 }
 
+const CONTRACT_ADDRESS = '0x41C9C4e683Bf46e7fcDB06a5F0f2b29938a90Fb5'
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [account, setAccount] = useState("");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [chain, setChain] = useState(0);
+
+  var provider: any, contract: any, signer: any 
+
+  const connectContract = async () => {
+    try {
+      provider = new ethers.providers.Web3Provider(window.ethereum)
+      signer = provider.getSigner()
+      contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer)
+    } catch(e) {
+      console.log(e)
+    }
+  }
 
   const checkWalletIsConnected = async () => {
     try {
@@ -79,6 +97,107 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   };
 
+  /// Getter Functions 
+
+  const getAllProjects = async () => {
+    try {
+      connectContract()
+      const projects = await contract.getAllProjects()
+      return projects;
+    } catch(e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  const getAllProposals = async (projectId: number) => {
+    try {
+      connectContract()
+      const proposals = await contract.getAllRequests(projectId)
+      return proposals
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  const getDetails = async (projectId: number) => {
+    try {
+      connectContract()
+      const details = await contract.getDetails(projectId)
+      return details
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  const usersContributions = async (projectId: number) => {
+    try {
+      connectContract()
+      if(account) {
+        const contributions = await contract.myContributions(projectId, account)
+        return contributions
+      } else {
+        console.log("throwitin: you are logged in")
+        return false
+      }
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  /// Setter Functions
+
+  const createProject = async (formData: Project) => {
+    try {
+      connectContract()
+      const project = await contract.startProject(formData.title, formData.fundRaisingDeadline, formData.goalAmount, formData.category, formData.img, formData.uri)
+      await project.wait()
+      return true
+    } catch(e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  const createProposal = async (projectId: number, desc: string, value: number, recipient: string) => {
+    try {
+      connectContract()
+      const proposal = await contract.createRequest(projectId, desc, value, recipient)
+      await proposal.wait()
+      return true
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  const voteOnProposal = async (projectId: number, proposalId: number, vote: boolean) => {
+    try {
+      connectContract()
+      const voting = await contract.voteRequest(projectId, proposalId, vote)
+      await voting.wait()
+      return true
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
+  const getRefund = async (projectId: number) => {
+    try {
+      connectContract()
+      const refund = await contract.getRefund(projectId)
+      await refund.wait()
+      return true
+    } catch (e) {
+      console.log(e)
+      return false
+    }
+  }
+
   // Check Ethereum Network & Switch
   useEffect(() => {
     const { ethereum } = window;
@@ -105,8 +224,24 @@ function MyApp({ Component, pageProps }: AppProps) {
     changeNetwork,
   };
 
+  const supportedChainIds = [80001];
+
+  const connectors = {
+    injected: {},
+    walletconnect: {},
+    walletlink: {
+      appName: "thirdweb - demo",
+      url: "https://thirdweb.com",
+      darkMode: false,
+    },
+  };
+
   return (
     <AppContext.Provider value={sharedState}>
+      <ThirdwebProvider 
+        connectors={connectors} 
+        supportedChainIds={supportedChainIds}
+      >
       <Head>
         <link
           href="http://fonts.cdnfonts.com/css/clash-display"
@@ -120,6 +255,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
+      </ThirdwebProvider>
     </AppContext.Provider>
   );
 }
